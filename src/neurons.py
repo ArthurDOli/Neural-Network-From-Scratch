cf1097e720fbc02e563b289bf8bffcee04ea6e41
Arthur Duarte Oliveira <arthur.duarte.oli06@gmail.com>
2025-11-01 14:48:13 -0300

    feat(neurons): Docstrings and typehints added

--- src/neurons.py
+++ src/neurons.py
@@ -1,26 +1,52 @@
 import numpy as np
+from typing import Union, TYPE_CHECKING
+if TYPE_CHECKING:
+    from numpy.typing import NDArray
 
 class Perceptron:
     """
-    x
+    Implements the fundamental binary decision mechanism of the Perceptron articifial neuron.
+
+    The Perceptron determines its output (0 or 1) based on whether the weighted sum of inputs 
+    exceeds a threshold (represented by the bias).
     """
 
-    def __init__(self, N):
+    def __init__(self, num_inputs: int):
         """
-        x
-        """
-        self.weight = np.random.randn(N, 1)
-        self.bias = np.random.randn(1, 1)
+        Initialized the Perceptron's parameters (weights and bias) randomly.
 
-    def activation_function(self, z) -> float:
+        Args:
+            num_imputs: The number of binary inputs the perceptron will receives.
         """
-        x
+        self.weight: 'NDArray[np.float64]' = np.random.randn(num_inputs, 1)
+        self.bias: 'NDArray[np.float64]' = np.random.randn(1, 1)
+
+    def activation_function(self, z: Union[float | 'NDArray[np.float64]']) -> float:
         """
-        return 1.0 if z > 0 else 0.0
+        The Perceptron's non-smooth binary step activation function.
+
+        Outputs 1.0 if the weighted input (z) is strictly greater than zero, otherwise outputs
+        0.0.
+
+        Args:
+            z: The weighted input (z = w * x + b)
+        
+        Returns:
+            The binary output (0.0 or 1.0)
+        """
+        z_scalar = z.item() if isinstance(z, np.ndarray) else z
+        return 1.0 if z_scalar > 0 else 0.0
     
-    def forward(self, x):
+    def forward(self, x: 'NDArray[np.float64]') -> float:
         """
-        x
+        Performs the forward propagation step: calculates t he weighted input and applies the
+        activation function to produce a binary output.
+
+        Args:
+            x: The input vector (N, 1). For the Perceptron, inputs are binary.
+
+        Returns:
+            The final binary output (0.0 or 1.0) of the neuron.
         """
         weighted_sum = np.dot(self.weight.T, x) + self.bias
         return Perceptron.activation_function(z=weighted_sum)
